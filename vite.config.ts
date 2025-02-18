eafa76573506adcb72e6ef3493b06e3386cb2033
Skynin <sohay_ua@yahoo.com>
2025-02-18 16:56:35 +0200

    minor

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 	base: '/',
+	build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
 	server: {
 		port: 5173,
     proxy: {
